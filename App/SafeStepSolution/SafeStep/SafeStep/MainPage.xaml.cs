ddd7e3554c41d513978d2c848d9a0187f110da40
jpcoker3 <111995337+jpcoker3@users.noreply.github.com>
2023-09-13 00:35:37 -0500

    Add App
    Add basic app utilizing Xamarin an Visual Studio. This should allow the app to be used with both IOS and Android. Thus far the app has been successfully emulated on Android. The app consists of a home page with a few buttons to navigate around to other blank pages. Next up is RFID and Bluetooth connectivity.

---
+++ App/SafeStepSolution/SafeStep/SafeStep/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SafeStep
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        int count = 0;
+        void Button_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+
+        // Navigate to settings page.
+        async void NavigateToSettings(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new SettingsPage()));
+        }
+
+        // Navigate to status page.
+        async void NavigateToStatus(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new StatusPage()));
+        }
+
+        // Navigate to Locate page.
+        async void NavigateToLocate(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new LocatePage()));
+        }
+
+        // Navigate to settings page.
+        async void NavigateToPair(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NavigationPage(new PairPage()));
+        }
+
+
+    }
+
+}
+
---
+++ App/SafeStepSolution/SafeStep/SafeStep/PairPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SafeStep
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PairPage : ContentPage
+	{
+		public PairPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
