ba72d33bf4ef4e698b34fd53860cc7126e54ee75
jpc496 <jpc496@msstate.edu>
2023-10-05 16:05:43 -0500

    Initial file creation
    Co-authored-by: ekm366 <ekm366@users.noreply.github.com>

---
+++ Teensy/main.ino
@@ -0,0 +1,20 @@
+/*
+This is the main .ino file to be used with the Safe Step senior design project.
+
+Authors are:
+    Joseph Coker
+    Emily Miller
+    Colby Stevens
+    Ryan Harper
+*/
+
+void setup(){
+    //Serial.begin(9600);
+}
+
+
+void loop(){
+
+
+
+}
