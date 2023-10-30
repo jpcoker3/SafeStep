0c835cf95834a7eaf91314aa86591067ee6693e0
jpcoker3 <111995337+jpcoker3@users.noreply.github.com>
2023-10-30 14:54:35 -0500

    Add NFC control

--- App/SafeStepMAUI/TabbedPage/App.xaml.cs
+++ App/SafeStepMAUI/TabbedPage/App.xaml.cs
@@ -6,6 +6,9 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
+        // Force light theme
+        UserAppTheme = AppTheme.Light;
+        
 		MainPage = new AppTabbedPage();
 	}
 }
