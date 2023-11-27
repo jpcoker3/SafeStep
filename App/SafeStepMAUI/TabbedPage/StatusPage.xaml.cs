6404bc099c812eda1105762a02b62d5d5f0b3644
jpcoker3 <111995337+jpcoker3@users.noreply.github.com>
2023-11-27 00:31:15 -0600

    Add bluetooth connectivity
    Added bluetooth support. Currently facing a bug with reading past end of stream but hope to fix shortly.

--- App/SafeStepMAUI/TabbedPage/StatusPage.xaml.cs
+++ App/SafeStepMAUI/TabbedPage/StatusPage.xaml.cs
@@ -1,10 +1,21 @@
 ﻿
+using Microsoft.Maui.Controls;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
 namespace TabbedPageSample;
 public partial class StatusPage : ContentPage
 {
+    private BluetoothClient client;
 	public StatusPage()
 	{
 		InitializeComponent();
+        client = new BluetoothClient();
+
 	}
 
     protected override void OnAppearing()
@@ -13,10 +24,12 @@ public partial class StatusPage : ContentPage
         setDecibel(75);
         setBatteryLevel(0.50);
         setTemperature(85);
-       
-       
 
-       
+        // Initiate Bluetooth connection
+        ConnectBluetooth();
+
+
+
     }
     public void setDecibel(float decibelReading) // any int
     {
@@ -87,15 +100,100 @@ public partial class StatusPage : ContentPage
         }
     }
 
-    public void connectBT()
+    private void ConnectBluetooth()
     {
+        // Discover and connect to the Bluetooth device
+        BluetoothDeviceInfo[] devices = (BluetoothDeviceInfo[])client.DiscoverDevices();
 
-        return;
+        BluetoothDeviceInfo device = null;
+
+        foreach (BluetoothDeviceInfo obj in devices)
+        {
+            if (obj.DeviceName != null && obj.DeviceName.Contains("HC-05"))
+            {
+                // You can select the device based on its name, address, or any other criteria
+                device = obj;
+            }
+        }
 
 
+        if (device != null)
+        {
+            client.Connect(device.DeviceAddress, BluetoothService.SerialPort);
+
+            // Start a new thread for data reception after connection
+            Thread receiveThread = new Thread(ReceiveData);
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+        }
+        else
+        {
+            // Handle device not found or connection failure
+        }
     }
 
+    private void ReceiveData()
+    {
+        var stream = client.GetStream();
+        byte[] receive = new byte[1024];
 
+        while (true)
+        {
+            Array.Clear(receive, 0, receive.Length);
+            var readMessage = "";
+            do
+            {
+                
+                int bytesRead = stream.Read(receive, 0, receive.Length);
+                readMessage += Encoding.ASCII.GetString(receive, 0, bytesRead);
+            }
+            while (stream.DataAvailable);
+
+
+            // at this point, the readMessage string will contain the entire message. 
+
+            // Split the received message by commas to extract values
+            string[] values = readMessage.Split(',');
+
+            string humidity = "", decibel = "", latitude = "", longitude = "";
+            bool fallen = false;
+
+            foreach (string value in values)
+            {
+                if (value.StartsWith("hum:"))
+                {
+                    humidity = value.Substring(4);
+                    System.Diagnostics.Debug.WriteLine($"Humidity: {humidity}");
+                }
+                else if (value.StartsWith("dec:"))
+                {
+                    decibel = value.Substring(4);
+                    System.Diagnostics.Debug.WriteLine($"Decibel: {decibel}");
+                }
+                else if (value.StartsWith("coords:"))
+                {
+                    string[] coords = value.Substring(7).Split('x');
+                    if (coords.Length == 2)
+                    {
+                        latitude = coords[0];
+                        longitude = coords[1];
+                        System.Diagnostics.Debug.WriteLine($"Latitude: {latitude}, Longitude: {longitude}");
+                    }
+                }
+                else if (value.StartsWith("fall:true"))
+                {
+                    fallen = true;
+                    System.Diagnostics.Debug.WriteLine("Fallen: true");
+                }
+                else if (value.StartsWith("fall:false"))
+                {
+                    fallen = false;
+                    System.Diagnostics.Debug.WriteLine("Fallen: false");
+                }
+            }
+
+        }
+    }
 
 }
 
