af6e4b27cfd13cd661e7e7b2b0b259339e665384
jpc496 <jpc496@msstate.edu>
2023-11-26 19:13:07 -0600

    Add Bluetooth perms

--- App/SafeStepMAUI/TabbedPage/StatusPage.xaml.cs
+++ App/SafeStepMAUI/TabbedPage/StatusPage.xaml.cs
@@ -1,5 +1,5 @@
-﻿namespace TabbedPageSample;
-
+﻿
+namespace TabbedPageSample;
 public partial class StatusPage : ContentPage
 {
 	public StatusPage()
@@ -10,36 +10,21 @@ public partial class StatusPage : ContentPage
     protected override void OnAppearing()
     {
         base.OnAppearing();
+        setDecibel(75);
+        setBatteryLevel(0.50);
+        setTemperature(85);
+       
+       
 
-        // Get the current battery level and update the progress bar
-        var batteryReading = 0.50;
+       
+    }
+    public void setDecibel(float decibelReading) // any int
+    {
 
-
-        batteryProgressBar.Progress = batteryReading; // between 0 and 1
-        batteryProgressLabel.Text = "Battery Level: " + batteryReading*100 + "%"; // copy the data from progress bar
-        //control battery color
-        if (batteryReading > 0.75)
-        {
-            batteryProgressBar.ProgressColor = Colors.Green;
-        }else if (batteryReading > 0.25)
-        {
-            batteryProgressBar.ProgressColor = Colors.Orange;
-        }
-        else
-        {
-            batteryProgressBar.ProgressColor = Colors.Red;
-        }
-
-
-
-
-
-        //get current decibel reading
-        var decibelReading = 75;
-        //Decibel reading betweel 60 -120 dB. 
+        //Decibel reading between 60 -120 dB. 
         var decibelProgress = Math.Round((float)decibelReading, 2);
 
-        decibelProgressBar.Progress = (double)decibelProgress/120; // 120 is max decibels. 
+        decibelProgressBar.Progress = (double)decibelProgress / 120; // 120 is max decibels. 
         decibelLabel.Text = "Decibel Level: " + decibelReading + " db"; // describe current decibels
 
         if (decibelProgress >= 0.85)
@@ -55,13 +40,15 @@ public partial class StatusPage : ContentPage
             decibelProgressBar.ProgressColor = Colors.Blue;
         }
 
-        // Get current Temperature Rating
-        var tempReading = 76;
+    }
+
+    public void setTemperature(float tempReading) // any int
+    {
 
         //we want a range from 65-75 on avg. we will expand to 55-85 for general purposes. 
         // number - 55: brings avg from 0 - 30. 
         //so (number -55)/30 should give us a range between 0-1 for the range 0 - 120.
-        var tempProgress = Math.Round(((float)tempReading/120), 2);
+        var tempProgress = Math.Round(((float)tempReading / 120), 2);
         temperatureProgressBar.Progress = (double)tempProgress;
         temperatureLabel.Text = "Heat Index: " + tempReading + "°F";
 
@@ -77,7 +64,39 @@ public partial class StatusPage : ContentPage
         {
             temperatureProgressBar.ProgressColor = Colors.Red;
         }
+
     }
+
+    public void setBatteryLevel(double batteryReading) // 0-1
+    {
+
+        batteryProgressBar.Progress = batteryReading; // between 0 and 1
+        batteryProgressLabel.Text = "Battery Level: " + batteryReading * 100 + "%"; // copy the data from progress bar
+        //control battery color
+        if (batteryReading > 0.75)
+        {
+            batteryProgressBar.ProgressColor = Colors.Green;
+        }
+        else if (batteryReading > 0.25)
+        {
+            batteryProgressBar.ProgressColor = Colors.Orange;
+        }
+        else
+        {
+            batteryProgressBar.ProgressColor = Colors.Red;
+        }
+    }
+
+    public void connectBT()
+    {
+
+        return;
+
+
+    }
+
+
+
 }
 
 
